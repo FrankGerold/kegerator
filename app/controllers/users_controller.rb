c0520c125d5339a40abae7107e549ade0bde6123
Frank Gerold <FrankETSGerold@gmail.com>
2019-12-03 17:44:39 -0500

    Finish user pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,18 +9,49 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
-    redirect_to @user
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to new_user_path(@user)
+    end
   end
 
   def show
-    @user = User.find(params[:id])
+    find_user
+  end
+
+  def edit
+    find_user
+  end
+
+  def update
+    find_user
+    # @user = User.new(user_params)
+    if @user.update
+      redirect_to @user
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to edit_user_path(@user)
+    end
+  end
+
+
+  def destroy
+    find_user
+    @user.destroy
   end
 
 
   private
 
-  def user_params
-      params.require(:user).permit(:name, :bio)
+  def find_user
+    @user = User.find(params[:id])
   end
+
+  def user_params
+    params.require(:user).permit(:name, :bio)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :cups
   has_many :kegs, through: :cups
+  validates :name,  presence: true
+  validates :bio,  presence: true
 end
